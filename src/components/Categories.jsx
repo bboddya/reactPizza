8bf78412ec13d6e90cbcaf6959d144d8120c353c
bogdan <b.myagkov@skyway.capital>
2022-05-23 00:31:15 +0300

    start react-pizza

---
+++ src/components/Categories.jsx
@@ -0,0 +1,16 @@
+function Categories(props) {
+  return (
+    <div class="categories">
+      <ul>
+        <li class="active">Все</li>
+        <li>Мясные</li>
+        <li>Вегетарианская</li>
+        <li>Гриль</li>
+        <li>Острые</li>
+        <li>Закрытые</li>
+      </ul>
+    </div>
+  );
+}
+
+export default Categories;
